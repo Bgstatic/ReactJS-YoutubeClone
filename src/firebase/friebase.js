480fd26cd157f80020583769d520b75f4332e758
Bgstatic <bilgehangecici@gmail.com>
2020-09-30 15:12:53 +0300

    Firebase added.

---
+++ src/firebase/friebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBB2VwJL26pkcV8LmpTyBCWJkLhJiJNqzc",
+  authDomain: "react--clone-a435b.firebaseapp.com",
+  databaseURL: "https://react--clone-a435b.firebaseio.com",
+  projectId: "react--clone-a435b",
+  storageBucket: "react--clone-a435b.appspot.com",
+  messagingSenderId: "638507110102",
+  appId: "1:638507110102:web:3713fe81b961a445304377",
+  measurementId: "G-ESSYJQQTM9",
+};
